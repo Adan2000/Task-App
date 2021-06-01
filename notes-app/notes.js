5544b3570d3c3aca7a3a61d20fbd58319df3c7df
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-01 00:01:10 -0700

    add notes es6

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,14 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function () {
+const getNotes = () => {
     return 'Your notes...'
 }
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
         notes.push({
