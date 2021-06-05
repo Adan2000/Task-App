6fffb5f8f69b853b5fe7214b07ffa0b46ba5c57d
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-04 23:47:06 -0700

    api request test

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,11 @@
 const request = require('request')
 
+const url = 'http://api.weatherstack.com/current?access_key=182566dc91d2ec6fbe9af50bed79be46&query=37.8267,-122.4233'
 
-
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
 
 
 //Call stack. 
