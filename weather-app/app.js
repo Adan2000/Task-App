3939d7edf306b1b58551d3e3a33b90da2d9b2650
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-04 23:04:53 -0700

    Call stacl, Node Apis, and Event loop

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,3 +9,11 @@ setTimeout(() => {
 }, 0)
 
 console.log('Stopping')
+
+//Call stack. 
+//main function gets pushed to the bottom of the call stack. This starts everything.
+//The rest of the functions onec they are called will get added to the call stack in order. 
+//In the call stack you cant execute two things at the same time. 
+//No asyncronous callbacks are ever going to run before our main function is done.
+//Call stack -> Node APIs -> Event Loop -> Call stack
+//Our Event loop cant run any asyncronous call backs until our Call Stack is empty.
