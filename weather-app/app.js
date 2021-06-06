33962b2c6d5dcd71c98858618598c21e497518c7
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-06 15:09:58 -0700

    lat / long

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,8 +2,22 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=182566dc91d2ec6fbe9af50bed79be46&query=37.8267,-122.4233&units=f'
 
+
+
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.current.weather_descriptions[0] + ', It is currently ' + response.body.current.temperature + ' degrees outside. It feels like ' + response.body.current.feelslike + ' degrees out.')
+})
+
+
+//Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaXN0aGlzYWRhbiIsImEiOiJja3BscGU4N3cwMjVkMnB1ZXc3MnBkMzB3In0.1Bgtj8BGPGofjmhH4oSVEg&limit=1'
+
+request({ url: geocodeurl, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
 })
 
 
