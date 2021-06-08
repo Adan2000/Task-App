24ffbbf17236d9c157ba8d8a7d32abd27af049b0
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-08 14:56:43 -0700

    remove notes

--- weather-app/app.js
+++ weather-app/app.js
@@ -13,13 +13,6 @@ geocode('Seattle', (error, data) => {
     console.log('Data', data)
 })
 
-//Call stack. 
-//main function gets pushed to the bottom of the call stack. This starts everything.
-//The rest of the functions onec they are called will get added to the call stack in order. 
-//In the call stack you cant execute two things at the same time. 
-//No asyncronous callbacks are ever going to run before our main function is done.
-//Call stack -> Node APIs -> Event Loop -> Call stack
-//Our Event loop cant run any asyncronous call backs until our Call Stack is empty.
 
 //npm -y 
 //this makes it default. 
