1d2148d3ef391027d0e26f9ead09f3ae5bfea07f
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-09 23:11:23 -0700

    es6 shortcut practice 2

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -1,4 +1,4 @@
-// object prperty shorthand
+// object property shorthand
 
 const name = 'Andrew'
 const userAge = 27
@@ -10,3 +10,22 @@ const user = {
 }
 
 console.log(user)
+
+// Object destructuring 
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201, 
+    salePrice: undefined
+}
+
+// const label = product.label
+// const stock = product.stock 
+
+const { label, stock, price, salePrice } = product
+
+console.log(label)
+console.log(stock)
+console.log(price)
+console.log(salePrice)
