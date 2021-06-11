30a961b64565777676740a8f438dbc61cdf7ea75
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-11 12:05:21 -0700

    static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,40 +1,22 @@
-const  express = require('express')
+const path = require("path");
+const express = require("express");
+const app = express();
 
-const app = express()
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-//HTML
-app.get('', (req, res ) => {
-    res.send('<h1>Weather<h/h1>')
-})
-//JSON
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Adan'
-    },
-    {
-        name: 'Sarah'
-    }])
-})
+app.use(express.static(publicDirectoryPath));
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
-
-app.get('/weather', (req, res) => {
-    res.send({
-        forcast: 'It is raining',
-        location: 'Seattle'
-    })
-})
-
-// app.com 
-// app.com/help
-// app.com/about
-// app.com/weather
+app.get("/weather", (req, res) => {
+  res.send({
+    forcast: "It is raining",
+    location: "Seattle",
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000')
-})
+  console.log("Server is up on port 3000");
+});
 
-//nodeman src/app.js 
+//nodemon src/app.js
 //Listens to changes
+// CMD -> SHIFT -> P (Format file)
