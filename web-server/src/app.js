12ee89790102edb031a488f4f867eea326db6623
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-11 10:38:46 -0700

    sending HTML & JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,26 +2,39 @@ const  express = require('express')
 
 const app = express()
 
+//HTML
 app.get('', (req, res ) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather<h/h1>')
 })
-
+//JSON
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([{
+        name: 'Adan'
+    },
+    {
+        name: 'Sarah'
+    }])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forcast: 'It is raining',
+        location: 'Seattle'
+    })
 })
 
 // app.com 
 // app.com/help
 // app.com/about
+// app.com/weather
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
+
+//nodeman src/app.js 
+//Listens to changes
