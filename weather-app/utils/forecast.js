3acdeae7ad814f3c73fb13c8f3d7b0d74254a822
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-10 22:14:24 -0700

    es6 destruct

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -4,13 +4,13 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=182566dc91d2ec6fbe9af50bed79be46&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service')
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ', It is currently ' + response.body.current.temperature + ' degrees outside. It feels like ' + response.body.current.feelslike + ' degrees out.'
+            callback(undefined, body.current.weather_descriptions[0] + ', It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees out.'
             )
         }
     })    
