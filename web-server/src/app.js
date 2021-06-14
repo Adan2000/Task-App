67576d7711784fa6c1563e4c93c990d04b42324f
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-13 22:28:20 -0700

    notes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,11 +2,16 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+
+// Define paths for Express config. 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, '../templates')
 
+// Setup handlebars engine and views location.
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
+
+// Setup static directory to serve.
 app.use(express.static(publicDirectoryPath));
 
 
