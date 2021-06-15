343326c4aa3c084b2df2ba26dc352798150dcfe4
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-14 22:13:11 -0700

    404 page for help page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -51,6 +51,10 @@ app.get("/weather", (req, res) => {
   });
 });
 
+app.get('/help/*', (req, res) => {
+  res.send('Help article not found')
+})
+
 app.get('*', (req, res) => {
   res.send('My 404 page')
 })
