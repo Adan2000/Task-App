b84d65ad0a14843509a6443dc074474088362e1c
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-16 23:43:16 -0700

    fix 2

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -52,6 +52,11 @@ app.get("/weather", (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
   res.send({
     products: []
   })
