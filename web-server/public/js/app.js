7fd5d4e7526c2c729b46aae0a1e0e8629edf1486
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-19 15:29:08 -0700

    fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side')
+
+fetch('http://puzzle.mead.io/puzzle')
+    .then((response) => {
+        response.json().then((data) =>{
+            console.log(data)
+        })
+})
+
+//Fetch JSON data from a URL.
+//Parse it into a JS object.
+//Console log that data
