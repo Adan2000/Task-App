18fd27068cab0363bea3df50eab8848a08280e96
Adan Escamilla <adanescamilla580@gmail.com>
2021-06-19 15:34:09 -0700

    using fetch for weather url

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,6 +7,20 @@ fetch('http://puzzle.mead.io/puzzle')
         })
 })
 
+const url = 'http://localhost:3000/weather?address=boston'
+
+fetch(url)
+    .then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+
 //Fetch JSON data from a URL.
 //Parse it into a JS object.
 //Console log that data
