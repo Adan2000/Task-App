9eae246e6c74258ff9f64c6871bfb8f7fd9e06dc
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-01 19:35:53 -0700

    connected

---
+++ task-manager/mongodb.js
@@ -0,0 +1,14 @@
+// CRUD 
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://localhost:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+    if (error) {
+       return console.log('Unable to connect to database')
+    }
+    console.log('Connected')
+})
