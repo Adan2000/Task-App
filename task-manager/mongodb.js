4d5c2c328ab7bca23cacbad6b45faec89730359e
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-02 21:50:23 -0700

    x

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,13 +12,13 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     }
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Adan',
-        age: 21
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user')
-        }
-        console.log(result.ops)
-    })
+    // db.collection('users').insertOne({
+    //     name: 'Adan',
+    //     age: 21
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+    //     console.log(result.ops)
+    // })
 })
