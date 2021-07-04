fbb9facc244cc0d12637ea839c99bbb1a4215a30
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-03 21:12:32 -0700

    objectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,6 +9,8 @@ const { MongoClient, ObjectID, } = require('mongodb')
 const connectionURL = 'mongodb://localhost:27017'
 const databaseName = 'task-manager'
 
+const id = new ObjectID()
+
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if (error) {
        return console.log('Unable to connect to database')
