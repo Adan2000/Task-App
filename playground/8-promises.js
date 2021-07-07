329aebd9656f18f2c60224652cf2a9bc92472bf6
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-06 21:09:56 -0700

    promise  ex

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -2,6 +2,7 @@ const doWorkPromise = new Promise((resolve, reject) => {
     setTimeout (() => {
         // resolve([7,4,1])
         reject('Things went wrong')
+        resolve([2,3,2])
     }, 2000)
 })
 
