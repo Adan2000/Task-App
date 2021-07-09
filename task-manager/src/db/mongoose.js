4183862f9ecc1f75d5f649da6a4b1d482af066ba
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-08 17:35:48 -0700

    setting up model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,3 +4,17 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true, 
     useCreateIndex: true
 })
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'Adan',
+    age: 21
+})
