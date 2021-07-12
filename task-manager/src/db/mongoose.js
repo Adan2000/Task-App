8327f3b4e17e7e199671c04014785bfbe0a1a5a6
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-11 23:00:38 -0700

    comment

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,23 +5,23 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    }, 
-    completed: {
-        type: Boolean
-    }
-})
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     }, 
+//     completed: {
+//         type: Boolean
+//     }
+// })
 
-const newTask = new Task({
-    description: 'Work on project',
-    completed: true 
-})
+// const newTask = new Task({
+//     description: 'Work on project',
+//     completed: true 
+// })
 
-newTask.save().then(() => {
-    console.log(newTask)
-}).catch((e) => {
-    console.log('Error', e)
-})
+// newTask.save().then(() => {
+//     console.log(newTask)
+// }).catch((e) => {
+//     console.log('Error', e)
+// })
 
